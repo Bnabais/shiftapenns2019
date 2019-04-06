2b02a66787b2fd31dead1a9a2739d537668017ca
bruno.nabais <bruno.nabais@present-technologies.com>
2019-04-06 03:44:41 +0100

    setting first layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div class="col-lg-12 home">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
@@ -8,6 +7,7 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 @Component({
   components: {
