422495f6472a5573b3cc2fdff8a218e69fee089c
Daniel <daniel.moreira@present-technologies.com>
2019-04-07 14:10:38 +0100

    remover scroll

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  background-image: url('./assets/bg.jpg');
-  background-repeat: no-repeat;
-  background-size:cover;
-  background-position: center center;
-  width: 100%;
-  height: 100vh;
-}
+  body {
+    overflow: hidden;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    background-image: url('./assets/bg.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center center;
+    width: 100%;
+    height: 100vh;
+  }
 
 </style>
